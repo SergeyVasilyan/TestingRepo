0ab66d9c9660b6a8604564f9e3855b496e5b51a0
AsaturGaloyan <galoyanasatur@gmail.com>
2020-12-02 22:08:40 -0800

    Author: AsaturGaloyan Description: Added new code and divided it on functions!

--- game.py
+++ game.py
@@ -1,6 +1,37 @@
 #!/usr/bin/python3
 import random
 
+def game(randNum,head,tail,helper):
+	while True:
+		ourNum=input("Please enter number from {}-{} \n" .format(head,tail))
+		if ourNum=="exit":
+			return
+		elif ourNum=="help":
+			print(helper)
+			continue
+		else:
+			try:
+				ourNum=int(ourNum)
+				if ourNum<100 and ourNum>999:
+					print("Please enter number from {}-{}" .format(head,tail))
+					continue
+				if ourNum==randNum:
+					print("Congratulations You guess the magic number! The number is {}" .format(ourNum))
+					break
+				elif ourNum<head or ourNum>tail:
+					print("Magic number is in {}-{} range!" .format(head,tail))
+					continue
+				elif ourNum<randNum:
+					head=ourNum
+					print("Magic number is in {}-{} range!" .format(head,tail))
+					continue
+				else:
+					tail=ourNum
+					print("Magic number is in {}-{} range!" .format(head,tail))
+					continue
+			except:
+				print("Please enter integer from 100-999 range")
+
 def main():
 	randNum=random.randrange(100,1000)
 	head=100
@@ -9,21 +40,5 @@ def main():
 Hello dear friend!
 For playing this game please enter number from 100 to 999.
 Your aim is to guess the magic number which our program is keep in mind"""
-	while true:
-		ourNum=input("Please enter number from 100-999")
-		if ourNum=="exit":
-			return
-		elif ourNum=="help":
-			print(helper)
-			continue
-		else
-			try:
-				ourNum=int(ourNum)
-				if ourNum==randNum:
-					print("Congratulations You guess the magic number! The number is {}" .formate(ourNum))
-					break
-				elif ourNum<randNum:
-					head=ourNum
-					print("Magic number is in {}-{} range!" .format(head,tail))
-					continue 
+	game(randNum,head,tail,helper)
 main()
