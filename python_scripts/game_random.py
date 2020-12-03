71a1f7e1b750463d084afa75ff2716fce0704fb4
LusineManukyan <LusineManukyan@github.com>
2020-12-03 10:40:42 +0400

    Author: Lusine Description:         -- Script game changed.

--- python_scripts/game_random.py
+++ python_scripts/game_random.py
@@ -1,33 +1,43 @@
 #! /usr/bin/python3
-
 import random
-import keyboard
 
-print("Welcome to the Number Game!")
-random_num=random.randrange(100, 1000)
-guess_num=0
-count=0
-key = 'ctrl c'
-HELP="""
-This is a guessing game: you need to choose a number from 100 to 999. The game ends when you find a number or enter the word 'quit' or bang on the keys with  Ctrl C """
-while guess_num != random_num:
-    guess_num=input("Input number ")
-    if guess_num == "quit" or keyboard.is_pressed(key):
-        quit()
-    elif guess_num == "help":
-        print(HELP)
-        continue
-    else:
-        while guess_num.isdigit() == False:
-            print("Invalid number")
-            guess_num=input("Input number ")
-        count += 1
-        guess_num=int(guess_num)
-        if guess_num == random_num:
-            print("Congratulations,you found the number.")
-            print('You used', count, 'attempts.')
-        elif guess_num > random_num:
-            print ("Give a lower number ")
-        else:
-            print("Give a higher number")
+def help_message():
+    print("This is a guessing game: you need to choose a number from 100 to 999.")
+    print("The game ends when you find a number.")
+    print("To exit the game enter the word 'quit' or press 'Ctrl+C'\n")
 
+def game_func(random_num):
+    guess_num=0
+    count=0
+    try:
+        while guess_num != random_num:
+            guess_num=input("Input number: ")
+            if guess_num == "quit":
+                quit()
+            elif guess_num == "help":
+                help_message()
+                continue
+            elif guess_num.isdigit() == False:
+                count += 1
+                print("Invalid number.\n")
+                continue
+            else:
+                count += 1
+                guess_num=int(guess_num)
+                if guess_num == random_num:
+                    print("Congratulations,you found the number.")
+                    print("You used {} attempts.".format(count))
+                    break
+                elif guess_num > random_num:
+                    print ("Give a lower number.\n")
+                else:
+                    print("Give a higher number.\n")
+    except KeyboardInterrupt:
+        print(' Game is interrupted')
+
+def main():
+    print("Welcome to the Number Game!\n")
+    random_number=random.randint(100, 999) 
+    game_func(random_number)
+
+main()
