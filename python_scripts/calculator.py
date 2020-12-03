65f086f9a16f6d3f76d974b956a7c5550694ab16
LusineManukyan <lusine@example.com>
2020-12-03 21:47:09 +0400

    Author: Lusine Description:         --Script calculator changed

--- python_scripts/calculator.py
+++ python_scripts/calculator.py
@@ -21,8 +21,32 @@ def factorial(n):
    else:
     return n*factorial(n-1)
 
-def func(number):
-        return int(number) == float (number)
+def check_int(number):
+        return int(number) == float(number)
+def arithmetic_operations(num1,num2,sign):
+    if sign == '+':
+       if check_int(sum(num1,num2))== True:
+           print("The result of the action: {}".format(int(sum(num1,num2))))
+       else:
+           print("The result of the action: {}".format(sum(num1,num2)))
+
+    elif sign == '-':
+        if check_int(subtract(num1,num2)) == True:
+            print("The result of the action: {}".format(int(subtract(num1,num2))))
+
+        else:
+            print("The result of the action: {}".format(sum(num1,num2)))
+
+    elif sign == '*':
+        if check_int(mult(num1,num2)) == True:
+            print("The result of the action: {}".format(int(mult(num1,num2))))
+        else:
+            print("The result of the action: {}".format(mult(num1,num2)))
+    else:
+       if check_int(division(num1,num2)) == True:
+           print("The result of the action: {}".format(int(divisin(num1,num2))))
+       else:
+           print("The result of the action: {}".format(division(num1,num2)))
 
 def main():
     list=['+','-','/','!','*']
@@ -30,38 +54,22 @@ def main():
         num1 = float(input("Enter the first number: "))
         sign = input("Enter sign of action: ")
         while list.count(sign) == 0:
-            print("Invalid number")
-            number1=input("Enter the first number:  ")
+            print("Sorry,but there is no operation for this sign,choose from these('+','-','/','!','*')")
+            sign=input("Enter sign of action:  ")
         if sign == '!' :
-            factorial(int(num1))
-            print("The result of the action: {}".format(factorial(num1)))
-            return
+            if int(num1) == 0:
+                print("The result of the action: 1")
+                return
+            elif int(num1) < 0:
+                print("Sorry, factorial does not exist for negative numbers")
+                return
+            else:
+                factorial(int(num1))
+                print("The result of the action: {}".format(int(factorial(num1))))
+                return
 
         num2 = float(input("Enter the second number: "))
-        if sign == '+':
-           if func(sum(num1,num2))== True:
-               print("The result of the action: {}".format(int(sum(num1,num2))))
-           else:
-               print("The result of the action: {}".format(sum(num1,num2)))
-
-        elif sign == '-':
-            if func(subtract(num1,num2)) == True:
-                print("The result of the action: {}".format(int(subtract(num1,num2))))
-
-            else:
-                print("The result of the action: {}".format(sum(num1,num2)))
-
-        elif sign == '*':
-            if func(mult(num1,num2)) == True:
-                print("The result of the action: {}".format(int(mult(num1,num2))))
-            else:
-                print("The result of the action: {}".format(mult(num1,num2)))
-        else:
-           if func(division(num1,num2)) == True:
-               print("The result of the action: {}".format(int(divisin(num1,num2))))
-           else:
-               print("The result of the action: {}".format(division(num1,num2)))
-
+        arithmetic_operations(num1,num2,sign)
     except:
         print("Invalid type") 
 main()
