3190ffdd166a0520d0b7b92a1e0dc8dd5c15e0fb
LusineManukyan <LusineManukyan@github.com>
2020-12-04 14:44:47 +0400

    Script calculator added

--- python_scripts/calculator.py
+++ python_scripts/calculator.py
@@ -1,14 +1,19 @@
 #! /usr/bin/python3
 
-# I do not see the descriptions of any functions.
+def help_messenge():
+    print("This Calculator program allow you to execute simple calculations such as")
+    print("addition,subtract,multiplication,division and factorial.")
+
+# This function calculates the sum of two numbers.
 def add(number_one, number_two):
     return number_one + number_two
 
-# Bla bla bla
+# This function calculates the subtract of two numbers.
 def subtract(number_one, number_two):
     return number_one - number_two
 
-# Bla bla bla
+# This function calculates the division of two numbers,
+# if a division occurs at zero, ZeroDivisionError exception is generated.
 def divide(number_one, number_two):
     try:
         result = number_one / number_two
@@ -16,77 +21,86 @@ def divide(number_one, number_two):
     except ZeroDivisionError:
         print("Cannot divide by zero!")
 
-# Bla bla bla
+# This function calculates the multiplication of two numbers.
 def multiply(number_one, number_two):
     return number_one * number_two
 
-# Bla bla bla
-def factorial(number): 
-   if number == 1: # What if number == 0 or number < 0?
-    return number
+# This function calculates the factorial of number.
+def factorial(number):
+   if number == 0:
+       return 1
+   elif number < 0:
+       print("Error: negative number is given for factorial calculation.")
+       return
    else:
-    return number * factorial(number - 1)
+       if number == 1:
+        return number
+       else:
+        return number * factorial(number - 1)
 
-# Bla bla bla
 def check_number_is_integer(number):
         return int(number) == float(number)
 
-# Bla bla bla 
+# This function execute arithmetic operations.
 def arithmetic_operations(number_one, number_two, operator):
     if operator == '+':
-       if check_number_is_integer(add(number_one, number_two)) == True: # What is the purpose of checking the result is integer or not????
-#                                 ^^^^^^^^^^^^^^^^^^^^^^^^^^ 
-           print("The result of the action: {}".format(int(add(number_one, number_two))))
-       else:                                          #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-           print("The result of the action: {}".format(add(number_one, number_two)))
-#                                                      ^^^^^^^^^^^^^^^^^^^^^^^^^^
-    elif operator == '-':
-        if check_number_is_integer(subtract(number_one, number_two)) == True:
-#                                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-            print("The result of the action: {}".format(int(subtract(number_one, number_two))))
-#                                                       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        else:
-            print("The result of the action: {}".format(add(number_one, number_two))) # Why we summing number instead of subtracting????
-#                                                       ^^^^^^^^^^^^^^^^^^^^^^^^^^
-    elif operator == '*':
-        if check_number_is_integer(multiply(number_one, number_two)) == True:
-#                                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-            print("The result of the action: {}".format(int(multiply(number_one, number_two))))
-#                                                       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        else:
-            print("The result of the action: {}".format(multiply(number_one, number_two)))
-#                                                       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    else:
-       if check_number_is_integer(divide(number_one, number_two)) == True:
-#                                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-           print("The result of the action: {}".format(int(divide(number_one, number_two))))
-#                                                      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+       temp=add(number_one, number_two)
+       if check_number_is_integer(temp) == True:
+           print("The result of the action: {}".format(int(add(temp))))
        else:
-           print("The result of the action: {}".format(divide(number_one, number_two)))
-#                                                      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+           print("The result of the action: {}".format(add(temp)))
+    elif operator == '-':
+        temp=subtract(number_one, number_two)
+        print(type(temp))
+        if check_number_is_integer(temp) == True:
+            print("The result of the action: {}".format(int(temp)))
+        else:
+            print("The result of the action: {}".format(temp))
+    elif operator == '*':
+        temp=multiply(number_one, number_two)
+        if check_number_is_integer(temp) == True:
+            print("The result of the action: {}".format(int(temp)))
+        else:
+            print("The result of the action: {}".format(temp))
+    else:
+       temp=divide(number_one, number_two)
+       if check_number_is_integer(temp) == True:
+           temp=divide(number_one, number_two)
+           print("The result of the action: {}".format(int(temp)))
+       else:
+           print("The result of the action: {}".format(temp))
 
-# Bla bla bla
+def check_number_is_float():
+    while True:
+        try:
+            number = input("Enter the number: ")
+            number = float(number)
+            return number
+        except:
+            if number == "exit":
+                quit()
+            elif number == "help":
+                help_messenge()
+            else:
+                print("Invalid number")
+
+# The main function into which the other functions are called.
 def main():
     list=['+','-','/','!','*']
     try:
-        number_one = float(input("Enter the first number: "))
+        number_one = check_number_is_float()
         operator = input("Enter the operator: ")
-        while list.count(operator) == 0: # Actually it works, but i don't like the logic. RE-IMPLEMENT!!!
-            print("Invalid operator is given\nChoose from ('+','-','/','!','*')")
-            operator = input("Enter the operator: ")
+        while operator not in list:
+          print("Invalid operator is given\nChoose from ('+','-','/','!','*')")
+          operator = input("Enter the operator: ")
         if operator == '!' :
-            if int(number_one) == 0:
-                print("The result of the action: 1") # ????????????  Is not this be the result of the function ???????????
-                return # <----- Change to 'break' statement.
-            elif int(number_one) < 0:
-                print("Error: negative number is given for factorial calculation.")
-                return # <----- This too.
-            else:
-                factorial(int(number_one)) # What is the purpose of this call??????
-                print("Result: {}".format(int(factorial(number_one))))
-                return # <----- aaaaand this too. :)
-        number_two = float(input("Enter the second number: "))
+            print("Result: {}".format(factorial(int(number_one))))
+            return
+        number_two = check_number_is_float()
         arithmetic_operations(number_one, number_two, operator)
-    except:
-        print("Invalid operator.") 
+    except Exception:
+        print("Invalid operator.")
+    except KeyboardInterrupt: 
+        print("\nProgram is interrupted.")
+
 main()
