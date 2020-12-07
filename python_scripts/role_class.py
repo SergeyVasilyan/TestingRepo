db5673b29f57788ef8279c8faa12183a4fdca2cd
LusineManukyan <lusine@example.com>
2020-12-07 23:50:57 +0400

    Author: Lusine Description:         --Script change.

--- python_scripts/role_class.py
+++ python_scripts/role_class.py
@@ -24,7 +24,7 @@ class data_user:
 
     def get_email(self):
         return self.__email
-        
+ 
     def set_role(self, role):
         self.__role = role
 
@@ -38,25 +38,37 @@ class Admin(data_user):
 
     def change_password(self, user = None):
         if user == None:
-            new_pass = input("Enter new password: ")
-            self.set_password(new_pass)
-            return
-        while True:
+            while True:
+                new_pass = input("Enter new password: ")
+                if len(new_pasis) >= 8:
+                    self.set_password(new_pass)
+                    return
+                else:
+                    print("Password must contain at least 8 characters,\nPlease try again.")
+                    continue
+        role = user.get_role()
+        while role == "guest" or role == "employee" :
             new_password = input("Enter new password: ")
             if len(new_password) >= 8:
-                 user.set_password(new_password)
-                 return
+                user.set_password(new_password)
+                return
             else:
                 print("Password must contain at least 8 characters,\nPlease try again.")
                 continue
+        print("You can not change a password for a {} as it role is {}.".format(user.get_name(), user.get_role()))
 
 class Employee(data_user):
 
     def change_password(self, user = None):
         if user == None:
-            new_pass = input("Enter new password: ")
-            self.set_password(new_pass)
-            return
+            while True:
+                new_pass = input("Enter new password: ")
+                if len(new_pasis) >= 8:
+                    self.set_password(new_pass)
+                    return
+                else:
+                    print("Password must contain at least 8 characters,\nPlease try again.")
+                    continue
         role = user.get_role()
         while role == "guest":
             new_password = input("Enter new password: ")
@@ -69,26 +81,15 @@ class Employee(data_user):
         print("You can not change a password for a {} as it role is {}.".format(user.get_name(), user.get_role()))
 
 class Guest(data_user):
-
-    def change_password(self,object_of_class=None,type=None):
-        pass
-#       while True:
-#           if not  object_of_class == None:
-#               self=object_of_class
-#               if type == 'guest':
-#                   password = input ("Please, input your password ")
-#               if password == object_of_class.get_password():
-#                   new_password = input("Enter new password ")
-#                   if len(new_password) >= 8:
-#                        object_of_class.set_password(new_password)
-#                        break
-#                   else:
-#                       print("Password must contain at least 8 characters ,please try again.")
-#                       continue
-#               else:
-#                   print ("You input wrong password.")
-#                   continue
-#           else:
-#               print("Guest cannot change admin and employee password ")
-#               break 
+    def change_password(self, user = None):
+        if user == None:
+            while True:
+                new_pass = input("Enter new password: ")
+                if len(new_pasis) >= 8:
+                    user.set_password(new_pass)
+                    return
+                else:
+                    print("Password must contain at least 8 characters,\nPlease try again.")
+                    continue 
+        print("You can not change a password for a {} as it role is {}.".format(user.get_name(), user.get_role()))
 
