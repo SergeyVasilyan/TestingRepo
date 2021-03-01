fd38cc8712422c186abac72669a0e71e018bb508
Lusine <lusinemanukian6@gmail.com>
2021-03-02 02:31:48 +0400

    laaaaaaaaaa

--- 3dmatrix_task/main.cpp
+++ 3dmatrix_task/main.cpp
@@ -8,26 +8,35 @@ int main()
   const int z = 1;
 
   char  myArray[x][y][z];
-
-//  std::ifstream fin("matrix2");
-  FILE *fp = fopen("matrix", "r");
-  for ( int i = 0; i < x; i++ )
-  {
-    for ( int j = 0; j < y; j++ )
-    {
-	for (int k = 0; k < z; ++k)
-    {
-	//fin >> myArray[i][j][k];
-
-	int num;
-	fscanf(fp, " %d", &num);
-        myArray[i][j][k]= num;
-	std::cout<<"toxe-"<<num<<std::endl;
-	std::cout<<"file-"<<fp<<std::endl;
-
+std::string str;
+ int num=0;
+  std::ifstream fin("matrix2");
+ 
+  for ( int i = 0; i < x; i++ ) {
+    for ( int j = 0; j < y; j++ ){
+	for (int k = 0; k < z; ++k) {
+        myArray[i][j][k]= '0';
     }
+   }
   }
-  }
+int j,i,t;
+ for ( t = 0; t < x; t++) {
+	  j = 0;
+        while (getline(fin, str) )
+        std::cout<<str<<std::endl;
+	{
+            for (i = 0; str[i] != '\0'; i++) {
+		if(str[i] == ' ')
+			continue;
+                if ((str[i] >= '0') && (str[i] <= '9')){std::cout<<"llllllllllll"<<std::endl;
+                  num= i * 10 + (str[i] - '0');
+                }
+		std::cout<<num<<std::endl;
+		myArray[t][j][0]= (char)num;
+		j++;
+                }
+            }
+        }
 
   for ( int i = 0; i < x; i++ )
   {
