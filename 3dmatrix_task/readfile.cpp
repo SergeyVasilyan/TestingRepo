fdfc18eb18d1a53df86cd7365bee6ce389ef0b94
Lus <lus@example.com>
2021-03-04 17:57:23 +0400

    blaaaaaaaa

--- 3dmatrix_task/readfile.cpp
+++ 3dmatrix_task/readfile.cpp
@@ -21,22 +21,22 @@ int main()
     std::ifstream fin("matrix2");
     std::ifstream fin1("matrix2");
 
+
+
 if(!fin.is_open()){
 	std::cout<<"file isn't open"<<std::endl;
 	    return -1;
 }
-
-
 while(getline(fin1,str1)){
 	std::cout << str1<<std::endl;
 	if(str1[0]!='\0'){colum_count=0;
 	row_count++;
 		for(int i=0; str1[i]!='\0';i++){
-			std::cout<<"str["<<i<<"]= "<<str[i]<<std::endl;
+			std::cout<<"str1["<<i<<"]= "<<str1[i]<<std::endl;
 			if(str1[i] == ' ' ){
 				if(i!=0 && str1[i-1] !=' '){
 					colum_count++;
-					std::cout<<"COUNT1- "<<colum_count<<" str["<<i<<"]= "<<str[i]<<std::endl;
+					std::cout<<"COUNT1- "<<colum_count<<" str1["<<i<<"]= "<<str1[i]<<std::endl;
 					i1=i;
 				}
 			}
@@ -55,35 +55,30 @@ if(row_count%X != 0 && row_count/X != Z){
 	std::cout<<"toxery qanaqy sxala"<<std::endl;
 	return -1;
 }
-
-
-
-
  std::cout<<"1111111"<<std::endl;
-
 int j, i, k=0, t=0;
   while (getline(fin, str) )
   { std::cout<<"2222222"<<std::endl;
 
 	  j=0;
 	  for ( i = 0; str[i] != '\0'; i++ ) { std::cout<<"333"<<std::endl;
-
 		  if ( str[i] >= '0' && str[i] <= '9') { 
 			  num= num * 10 + (str[i] - '0');
 		  }
-
-		  else if( str[i] == ' ' ){ std::cout<<"44444"<<std::endl;
-
+		  else if( str[i] == ' ' ){ std::cout<<"4"<<std::endl;
 			  myArray[t][j][k] = num ;
 			  j++;
 			  num=0;
+			  std::cout<<"4444444"<<std::endl;
+
 		  }
 		  else{
 			  std::cout<<"error"<<std::endl;
 			  return 1;
 		  }
-	  }
-	  if( str[i] == '\0' ){std::cout<<"5"<<std::endl;
+	  }std::cout<<"5"<<std::endl;
+
+	  if( str[i] == '\0' ){std::cout<<"55555"<<std::endl;
 
 		  myArray[t][j][k] = num ;
 		  ++t;
@@ -96,7 +91,6 @@ int j, i, k=0, t=0;
   }
   for ( int i = 0; i < X; i++ )
   {std::cout<<"6"<<std::endl;
-
 	  for ( int j = 0; j < Y; j++ ){
 		  for (int k = 0; k < Z; ++k)
 		  {
