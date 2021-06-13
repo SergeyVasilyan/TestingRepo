d0db36d4a3aa2816cbfe848fa220b1539ea59bee
Lusine <lusinemanukian6@gmail.com>
2021-06-14 01:40:28 +0400

    Author: Lusine Description: 	- Started implementation test environment. 	- Added unit-test: 		- 'test_assignment_operator',         	- 'test_equality_operator',           	- 'test_sum_operator',         	- 'test_sub_operator',         	- 'test_scalar_with_assignment_operator',          	- 'test_multiplication_number',         	- ' test_subscription_operator'.

--- c++_lesson/task1/src/matrix/main.cpp
+++ c++_lesson/task1/src/matrix/main.cpp
@@ -1,70 +1,65 @@
 #include"matrix.h"
 #include<iostream>
 #include<csignal>
-#include<pthread.h>
-#include<stdio.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<unistd.h>
-#include<fstream>
 #include<cstring>
-#define ERROR_CREATE_THREAD -11
-#define ERROR_JOIN_THREAD -12
-#define SUCCESS 0
-#define PORT 6996
+#include<cassert>
+
 void signal_handel(int signum)
 {
 	std::cout << "Interrupt signal (" << signum <<") received." << std::endl;
 	exit(signum);
 }
 
-void check_assignment_operator()
+void test_assignment_operator()
 {
-	Matrix mat1(3,2);
-	std::cin >> mat1;
-	mat1 = mat1;
-	std::cout << mat1 << std::endl;
+	Matrix mat1(3, 2);
+	Matrix mat2(3, 2);
+	mat2[2][1] = 10;
+	mat1 = mat2;
+	assert(mat1 == mat2);
 }
 
-void check_equality_operator()
+void test_equality_operator()
 {
-	Matrix mat1(3,2);
-	Matrix mat2(5,9);
-	if (mat1 == mat2) {
-		std::cout << " Matrices are equal. " <<std::endl;
-	}
-	std::cout << " Matrices aren't equal. " <<std::endl;
+	Matrix mat1(3, 2);
+	Matrix mat2(3, 2);
+	assert(mat1 == mat2);
 }
 
-void bad_check_sum_operator()
+void test_subscription_operator()
 {
-	Matrix mat1(3,2);
-	std::cin >> mat1;
-	Matrix mat2(5,9);
-	Matrix mat3 = mat1 + mat2;
-	std::cout << mat3 << std::endl;
+	double value = 2;
+	Matrix mat1(3, 2);
+	mat1[1][1] = value;
+	assert(mat1[1][1] == value);
+}//stugel erb int,char,string depqery;
+
+void test_sum_operator()
+{
+	Matrix mat1(3, 2);
+        mat1[2][1] = 9;
+	Matrix mat2(3, 2);
+	mat2[2][1] = 10;
+	Matrix mat3(3, 2);
+        mat3[2][1] = 19;
+	assert(mat3 == mat1 + mat2);
 }
 
-void check_sub_operator()
+void  test_sub_operator()
 {
-	Matrix mat1(3,2);
-	std::cin >> mat1;
-	Matrix mat2(3,2);
+	Matrix mat1(3, 2);
+	Matrix mat2(3, 2);
+	mat1[0][0] = 3;
+        mat1[2][1] = 5;
 	mat2[0][0] = 3;
 	mat2[2][1] = 5.222;
-	Matrix mat3 = mat1 - mat2;
-	std::cout << mat3 << std::endl;
+	Matrix mat3(3, 2);
+       	mat3[0][0] = 0;
+        mat3[2][1] = -0.222;
+ 	assert(mat3 == mat1 - mat2);
 }
 
-void bad_check_scalar_with_assignment_operator()
-{
-	Matrix mat1(3,2);
-	std::cin >> mat1;
-	mat1 *= mat1;
-	std::cout << mat1 << std::endl;
-}
-
-void check_scalar_with_assignment_operator()
+void test_scalar_with_assignment_operator()
 {
 	Matrix mat1(2,2);
 	mat1[0][0] = 5;
@@ -72,149 +67,69 @@ void check_scalar_with_assignment_operator()
 	mat1[1][0] = 2;
 	mat1[1][1] = 7;
 	mat1 *= mat1;
-	std::cout << mat1 << std::endl;
+	Matrix mat2(2,2);
+        mat2[0][0] = 27;
+        mat2[0][1] = 12;
+        mat2[1][0] = 24;
+        mat2[1][1] = 51;
+	assert(mat2 == mat1);
 }
-void check_multiplication_number()
+/* aha tvabanutyun
+//log generacnel u stugel assertov trela te che 
+void test_scalar_with_assignment_operator_failure()
 {
-	/*	
-		Matrix mat1(2,2);
-		mat1[0][0] = 1;
-		mat1[0][1] = 1;
-		mat1[1][0] = 1;
-		mat1[1][1] = 1;
-		mat1 = 3 * mat1;
-		mat1 = mat1 * 6;
-		std::cout << mat1 << std::endl;
-	/*
-	 */
-	Matrix mat1(2,2);
-	mat1[0][0] = 1;
-	mat1[0][1] = 1;
-	mat1[1][0] = 1;
-	mat1[1][1] = 1;
-	double a;
-	std::cin >> a;
-	mat1 = a * mat1;
-	std::cout << mat1 << std::endl;
+	Matrix mat1(3,2);
+	mat1[2][0] = 8;
+        mat1[1][1] = 10;
+        mat1[1][0] = 4;
+	mat1 *= mat1;
+}
+*/
 
-}
-void check_cin_operator()
+void test_multiplication_number()
 {
-	Matrix mat1(3,3); // stugel chaperi anpatasxanutyuny ev convert chlinely 
+	int matrix_size = 2;
+	int init_value = 1;
+	double multiplicator = 6;
+	Matrix mat1(matrix_size, matrix_size);
+	Matrix mat2(matrix_size, matrix_size);
+	for (int i = 0; i < matrix_size; i++){
+		for (int j = 0; j < matrix_size; j++){
+			mat1[i][j] = init_value;
+			mat2[i][j] = mat1[i][j] * multiplicator;
+		}
+	}
+	assert(mat2 == multiplicator * mat1);
+}
+
+//stugel en depqy erb matrixy bazmapatlaca int, char ,string(voch double)
+/*
+void check_cin_operator_failure()
+{
+	Matrix mat1(3,3); 
 	std::cout << "object created." << std::endl;
 	std::cin >> mat1;
 }
-bool check_double(const std::string& str)
-{
-	try {
-		double result = std::stod(str);
-		return true;
-	} catch (const std::invalid_argument& ia) {
-		return false;
-	}
-}
-bool check_data(std::string str, bool flag)
-{
-	std::string row = "";
-	int count = 1;
-	std::string word = "";
-	for (auto x : str) {
-		if (x == ' ') {
-			if(check_double(word)) {
-				if(count == 1) {
-					row = word;
-					count++;
-				}
-				word = "";
-			} else {
-				return false;
-			}
-		} else {
-			word = word + x;
-		}
-	}
-	if(word != "") {
-		if( ! check_double(word)) {
-			return false;
-		}
-	}
-	if (flag == true) {
-		return row == word;
-	}
-	return true;
-}
-void* server(void *args)
-{
-	int server_fd, new_socket, new1_socket;
-	struct sockaddr_in address;
-	int addrlen = sizeof(address);
-	char buffer[1024] = {0};
-	if ((server_fd = socket (AF_UNIX, SOCK_STREAM, 0)) == 0) {
-		perror("Socket failed.");
-		exit(EXIT_FAILURE);
-	}
-	address.sin_family = AF_UNIX;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(PORT);
+*/
 
-	if (bind (server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-		perror("Bind failed.");
-		exit(EXIT_FAILURE);
-	}
-	if (listen (server_fd, 1) < 0) {
-		perror("Listen failed.");
-		exit(EXIT_FAILURE);
-	}
-	if ((new_socket = accept (server_fd, (struct sockaddr *)&address,
-							  (socklen_t*)&addrlen)) < 0) {
-		perror("Accept failed.");
-		exit(EXIT_FAILURE);
-	}
-	if (recv (new_socket, buffer, strlen(buffer), 0) > 0) {
-		if (check_data(buffer, true)) {
-			if (recv (new_socket, buffer, strlen(buffer), 0) > 0) {
-				if (check_data(buffer, false)) {
-					strcpy(buffer, "Correct." );
-					send(new_socket, buffer, strlen(buffer), 0);
-				} else {
-					strcpy(buffer, "Incorrect." );
-					send(new_socket, buffer, strlen(buffer), 0);
-				}
-			} else {
-				printf("\nServer don't work.\n");
-			}
-		} else {
-			strcpy(buffer, "Incorrect" );
-			send(new_socket, buffer, strlen(buffer), 0);
-		}
-	} else {
-		printf("\nServer don't work.\n");
-	}
-	return SUCCESS;
+
+void test()
+{
+	 test_assignment_operator();
+         test_equality_operator();
+         test_sum_operator();
+         test_sub_operator();
+        // test_scalar_with_assignment_operator_failure();
+         test_scalar_with_assignment_operator();
+         test_multiplication_number();
+        // check_cin_operator_failure();
+         test_subscription_operator();
+
+
 }
 int main()
 {
 	signal(SIGABRT, signal_handel);
-	pthread_t thread;
-	int status = 0;
-	int status_addr = 0;
-	status = pthread_create(&thread, NULL, server, NULL);
-	if (status != 0) {
-		printf("Main error: can't create thread, status = %d\n", status);
-		exit(ERROR_CREATE_THREAD);
-	}
-	status = pthread_join(thread, (void**)&status_addr);
-	if (status != SUCCESS) {
-		printf("Main error: can't join thread, status = %d\n", status);
-		exit(ERROR_JOIN_THREAD);
-	}
-	//  check_assignment_operator();
-	//  check_equality_operator();
-	//  bad_check_sum_operator();
-	check_sub_operator();
-	//  bad_check_scalar_with_assignment_operator();
-	//  check_scalar_with_assignment_operator();
-	//  check_multiplication_number();
-	//  check_cin_operator();
+	test();
 	return 0;
 }
