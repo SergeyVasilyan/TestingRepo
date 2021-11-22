a1ec0ff6a4708bd6a717135d77b9b3d010e2143f
Sergey Vasilyan <sergey_vasilyan@instigatedesign.com>
2021-11-22 14:37:08 +0400

    Author: Sergey Description:         - Updated main solution:             - added fullscreen mode.

--- traffic_control.py
+++ traffic_control.py
@@ -4,9 +4,11 @@ import os
 import sys
 import cv2
 import time
+import glob
 import argparse
 from imutils.video import FPS
 from imutils.video import VideoStream
+from Xlib.display import Display
 
 RUNNING_ON_RPI = False
 os_info = os.uname()
@@ -33,6 +35,8 @@ log_levels = {
                 'INFO' : 1,
                 'DEBUG' : 0
              }
+
+WINDOW_NAME = "Traffic Control"
 
 def colorize_text(log_level):
     """
@@ -94,12 +98,12 @@ def predict(frame, net, confidence):
     for detection in out.reshape(-1, 7):
         image_id, label, conf, x_min, y_min, x_max, y_max = detection
         if conf > confidence:
-            pred_boxpts = ((x_min, y_min), (x_max, y_max))
-            prediction = (label, conf, pred_boxpts)
+            box_points = ((x_min, y_min), (x_max, y_max))
+            prediction = (label, conf, box_points)
             predictions.append(prediction)
     return predictions
 
-def print_label(label, image_for_result, x, y, frame_shape, pred_conf):
+def draw_on_frame(label, image_for_result, x, y, frame_shape, prediction_confidence):
         height = frame_shape[0]
         width = frame_shape[1]
         x_min = int(x[0] * width)
@@ -120,7 +124,7 @@ def print_label(label, image_for_result, x, y, frame_shape, pred_conf):
         else:
             label = "Other"
             color = (0, 255, 0)
-        label += ": {:.2f}%".format(pred_conf * 100)
+        label += ": {:.2f}%".format(prediction_confidence * 100)
         cv2.rectangle(image_for_result,
                       (x_min, y_min),
                       (x_max, y_max),
@@ -134,13 +138,20 @@ def print_label(label, image_for_result, x, y, frame_shape, pred_conf):
                     color,
                     2)
 
-def start_process(args, net, vs, show_flag, input_file, fps):
+def get_screen_size():
+    screen = Display(os.environ["DISPLAY"]).screen()
+    screen_width, screen_height = screen.width_in_pixels, screen.height_in_pixels
+    return screen_width, screen_height
+
+def start_process(args, net, stream, fps, show_flag, input_file, fullscreen_mode):
     writer = None
     confidence = args['confidence']
     output_flag = args['output']
+    if fullscreen_mode:
+        screen_width, screen_height = get_screen_size()
     while True:
         try:
-            frame = vs.read()
+            frame = stream.read()
             frame = frame[1] if input_file else frame
             frame_height, frame_width, _ = frame.shape
             image_for_result = frame.copy()
@@ -153,27 +164,33 @@ def start_process(args, net, vs, show_flag, input_file, fps):
                                           frame.shape[0]),
                                          True)
             predictions = predict(frame, net, confidence)
-            for i, pred in enumerate(predictions):
-                label, pred_conf, pred_boxpts = pred
-                ((x_min, y_min), (x_max, y_max)) = pred_boxpts
+            for index, predictions in enumerate(predictions):
+                label, prediction_confidence, box_points = predictions
+                ((x_min, y_min), (x_max, y_max)) = box_points
                 log("DEBUG", "Prediction #%s: confidence: %s, boxpoints: %s" %\
-                             (i, pred_conf, pred_boxpts))
-                if pred_conf > confidence:
+                             (index, prediction_confidence, box_points))
+                if prediction_confidence > confidence:
                     log("DEBUG", "Prediction #%s: confidence: %s, boxpoint: %s" %\
-                                 (i, pred_conf, pred_boxpts))
+                                 (index, prediction_confidence, box_points))
                     if show_flag:
-                        print_label(label,
-                                    image_for_result,
-                                    (x_min, x_max),
-                                    (y_min, y_max),
-                                    (frame_height, frame_width),
-                                    pred_conf)
+                        draw_on_frame(label,
+                                      image_for_result,
+                                      (x_min, x_max),
+                                      (y_min, y_max),
+                                      (frame_height, frame_width),
+                                      prediction_confidence)
             if not isinstance(writer, type(None)):
                 writer.write(image_for_result)
             if show_flag:
-                cv2.imshow("Output", image_for_result)
+                if fullscreen_mode:
+                    image_for_result = cv2.resize(image_for_result,
+                                                  (screen_width, screen_height),
+                                                  interpolation = cv2.INTER_AREA)
+                cv2.imshow(WINDOW_NAME, image_for_result)
                 key = cv2.waitKey(1) & 0xFF
                 if key == ord("q"):
+                    break
+                if cv2.getWindowProperty(WINDOW_NAME, cv2.WND_PROP_ASPECT_RATIO) < 0:
                     break
             fps.update()
         except KeyboardInterrupt:
@@ -202,33 +219,45 @@ def check_optimization():
 
 def get_input(input_file):
     if not input_file:
+        camera_index = 6
         log("WARNING", "starting video stream.")
-        return VideoStream(src = 6).start()
+        return VideoStream(src = camera_index).start()
     log("WARNING", "opening video file.")
     return cv2.VideoCapture(input_file)
 
+def check_app_mode(fullscreen_mode):
+    if not fullscreen_mode:
+        cv2.namedWindow(WINDOW_NAME)
+    else:
+        cv2.namedWindow(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN)
+        cv2.setWindowProperty(WINDOW_NAME,cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
 def parse_arguments():
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-c", "--confidence",
-                    default = .5,
-                    help = "Confidence threshold value. Default is '0.5'.")
-    ap.add_argument("-s", "--show",
-                    type = int,
-                    default = 0,
-                    help = "Switch to display image on screen. Default is set 0 aka no-display mode.")
-    ap.add_argument("-d", "--debug",
-                    type = int,
-                    default = 1,
-                    help = "Log message level. Default is set to 1, i.e. no 'DEBUG' level.")
-    ap.add_argument("-i", "--input",
-                    type = str,
-                    default = "",
-                    help = "Path to optional input video file.")
-    ap.add_argument("-o", "--output",
-                    type = str,
-                    default = 0,
-                    help = "Path to optional output video file.")
-    args = vars(ap.parse_args())
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--confidence",
+                        default = .6,
+                        help = "Confidence threshold value. Default is '0.6'.")
+    parser.add_argument("-s", "--show",
+                        type = int,
+                        default = 0,
+                        help = "Switch to display image on screen. Default is set 0 aka no-display mode.")
+    parser.add_argument("-d", "--debug",
+                        type = int,
+                        default = 1,
+                        help = "Log message level. Default is set to 1, i.e. no 'DEBUG' level.")
+    parser.add_argument("-i", "--input",
+                        type = str,
+                        default = "",
+                        help = "Path to optional input video file.")
+    parser.add_argument("-o", "--output",
+                        type = str,
+                        default = 0,
+                        help = "Path to optional output video file.")
+    parser.add_argument("-f", "--fullscreen",
+                        type = int,
+                        default = 0,
+                        help = "Show frame in fullscreen. Default value is set to 0, i.e. not fullscreen.")
+    args = vars(parser.parse_args())
     return args
 
 def main():
@@ -237,18 +266,26 @@ def main():
     show_flag = args['show']
     log_level = args['debug']
     input_file = args['input']
+    fullscreen_mode = args['fullscreen']
     check_optimization()
+    check_app_mode(fullscreen_mode)
     net = load_model()
-    vs = get_input(input_file)
+    stream = get_input(input_file)
     fps = FPS().start()
-    start_process(args, net, vs, show_flag, input_file, fps)
+    start_process(args,
+                  net,
+                  stream,
+                  fps,
+                  show_flag,
+                  input_file,
+                  fullscreen_mode)
     fps.stop()
     if show_flag:
         cv2.destroyAllWindows()
     if not input_file:
-        vs.stop()
+        stream.stop()
     else:
-        vs.release()
+        stream.release()
     log("INFO", "Elapsed time: {:.2f}".format(fps.elapsed()))
     log("INFO", "Approximate FPS: {:.2f}".format(fps.fps()))
 
