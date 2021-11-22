708abb4216f9cfba8ab9b46054400a6d540ce214
Sergey Vasilyan <sergey_vasilyan@instigatedesign.com>
2021-11-22 13:27:20 +0400

    Author: Sergey Description:         - Updated main solution, mineralized global variable usage.

--- traffic_control.py
+++ traffic_control.py
@@ -8,6 +8,11 @@ import argparse
 from imutils.video import FPS
 from imutils.video import VideoStream
 
+RUNNING_ON_RPI = False
+os_info = os.uname()
+if os_info[4][:3] == 'arm':
+    RUNNING_ON_RPI = True
+
 class text_colors:
     UNDERLINE = '\033[4m'
     WARNING = '\033[33m'
@@ -20,15 +25,7 @@ class text_colors:
     RED = '\033[31m'
     END = '\033[0m'
 
-RUNNING_ON_RPI = False
-os_info = os.uname()
-if os_info[4][:3] == 'arm':
-    RUNNING_ON_RPI = True
-
-W = None
-H = None
 log_level = None
-
 log_levels = {
                 'CRITICAL' : 4,
                 'ERROR' : 3,
@@ -36,70 +33,6 @@ log_levels = {
                 'INFO' : 1,
                 'DEBUG' : 0
              }
-
-def predict(frame, net, confidence):
-    '''
-    Prepare input blob and perform an inference
-    '''
-    w = h = 512
-    blob = cv2.dnn.blobFromImage(frame,
-                                 size = (w, h),
-                                 ddepth=cv2.CV_8U)
-    net.setInput(blob)
-    out = net.forward()
-    predictions = []
-    # The net outputs a blob with the shape: [1, 1, N, 7], where N is the number of detected bounding boxes.
-    # For each detection, the description has the format: [image_id, label, conf, x_min, y_min, x_max, y_max]
-    for detection in out.reshape(-1, 7):
-        image_id, label, conf, x_min, y_min, x_max, y_max = detection
-        if conf > confidence:
-            pred_boxpts = ((x_min, y_min), (x_max, y_max))
-            prediction = (label, conf, pred_boxpts)
-            predictions.append(prediction)
-    return predictions
-
-def print_label(label, image_for_result, x_min, x_max, y_min, y_max, pred_conf):
-        x_min = int(x_min * W)
-        y_min = int(y_min * H)
-        x_max = int(x_max * W)
-        y_max = int(y_max * H)
-        offset = 15
-        y = y_min - offset if y_min - offset > offset else y_min + offset
-        if label == 0:
-            label = "Bike"
-            color = (255, 0, 0)
-        elif label == 1.0:
-            label = "Car"
-            color = (0, 0, 255)
-        elif label == 2.0:
-            label = "Person"
-            color = (0, 255, 0)
-        else:
-            label = "Other"
-            color = (0, 255, 0)
-        label += ": {:.2f}%".format(pred_conf * 100)
-        cv2.rectangle(image_for_result,
-                      (x_min, y_min),
-                      (x_max, y_max),
-                      color,
-                      2)
-        cv2.putText(image_for_result,
-                    label,
-                    (x_min, y),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    1,
-                    color,
-                    2)
-
-def load_model():
-    MODEL_NAME = "person-vehicle-bike-detection-crossroad-1016"
-    MODEL_PATH = "/opt/intel/openvino_2021/deployment_tools/open_model_zoo/tools/downloader/intel"
-    MODEL_PATH = "./models"
-    GRAPH_FILENAME = MODEL_PATH + "/" + MODEL_NAME + "/FP32/"
-    GRAPH_XML = GRAPH_FILENAME + MODEL_NAME + ".xml"
-    GRAPH_BIN = GRAPH_FILENAME + MODEL_NAME + ".bin"
-    net = cv2.dnn.readNet(GRAPH_XML, GRAPH_BIN)
-    return net
 
 def colorize_text(log_level):
     """
@@ -143,6 +76,137 @@ def log(level, message):
         current_date = text_colors.HEADER + current_date + text_colors.END
         print("[%s] [%s]: %s" % (current_date, level, message))
 
+def predict(frame, net, confidence):
+    '''
+    Prepare input blob and perform an inference
+    '''
+    input_width = 512
+    input_height = 512
+    blob = cv2.dnn.blobFromImage(frame,
+                                 size = (input_width,
+                                         input_height),
+                                 ddepth = cv2.CV_8U)
+    net.setInput(blob)
+    out = net.forward()
+    predictions = []
+    # The net outputs a blob with the shape: [1, 1, N, 7], where N is the number of detected bounding boxes.
+    # For each detection, the description has the format: [image_id, label, conf, x_min, y_min, x_max, y_max]
+    for detection in out.reshape(-1, 7):
+        image_id, label, conf, x_min, y_min, x_max, y_max = detection
+        if conf > confidence:
+            pred_boxpts = ((x_min, y_min), (x_max, y_max))
+            prediction = (label, conf, pred_boxpts)
+            predictions.append(prediction)
+    return predictions
+
+def print_label(label, image_for_result, x, y, frame_shape, pred_conf):
+        height = frame_shape[0]
+        width = frame_shape[1]
+        x_min = int(x[0] * width)
+        y_min = int(y[0] * height)
+        x_max = int(x[1] * width)
+        y_max = int(y[1] * height)
+        offset = 15
+        y = y_min - offset if y_min - offset > offset else y_min + offset
+        if label == 0:
+            label = "Bike"
+            color = (255, 0, 0)
+        elif label == 1.0:
+            label = "Car"
+            color = (0, 0, 255)
+        elif label == 2.0:
+            label = "Person"
+            color = (0, 255, 0)
+        else:
+            label = "Other"
+            color = (0, 255, 0)
+        label += ": {:.2f}%".format(pred_conf * 100)
+        cv2.rectangle(image_for_result,
+                      (x_min, y_min),
+                      (x_max, y_max),
+                      color,
+                      2)
+        cv2.putText(image_for_result,
+                    label,
+                    (x_min, y),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    color,
+                    2)
+
+def start_process(args, net, vs, show_flag, input_file, fps):
+    writer = None
+    confidence = args['confidence']
+    output_flag = args['output']
+    while True:
+        try:
+            frame = vs.read()
+            frame = frame[1] if input_file else frame
+            frame_height, frame_width, _ = frame.shape
+            image_for_result = frame.copy()
+            if output_flag and isinstance(writer, type(None)):
+                fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+                writer = cv2.VideoWriter("Output.mp4",
+                                         fourcc,
+                                         30,
+                                         (frame.shape[1],
+                                          frame.shape[0]),
+                                         True)
+            predictions = predict(frame, net, confidence)
+            for i, pred in enumerate(predictions):
+                label, pred_conf, pred_boxpts = pred
+                ((x_min, y_min), (x_max, y_max)) = pred_boxpts
+                log("DEBUG", "Prediction #%s: confidence: %s, boxpoints: %s" %\
+                             (i, pred_conf, pred_boxpts))
+                if pred_conf > confidence:
+                    log("DEBUG", "Prediction #%s: confidence: %s, boxpoint: %s" %\
+                                 (i, pred_conf, pred_boxpts))
+                    if show_flag:
+                        print_label(label,
+                                    image_for_result,
+                                    (x_min, x_max),
+                                    (y_min, y_max),
+                                    (frame_height, frame_width),
+                                    pred_conf)
+            if not isinstance(writer, type(None)):
+                writer.write(image_for_result)
+            if show_flag:
+                cv2.imshow("Output", image_for_result)
+                key = cv2.waitKey(1) & 0xFF
+                if key == ord("q"):
+                    break
+            fps.update()
+        except KeyboardInterrupt:
+            break
+        except AttributeError:
+            break
+
+def load_model():
+    MODEL_NAME = "person-vehicle-bike-detection-crossroad-1016"
+    MODEL_PATH = "/opt/intel/openvino_2021/deployment_tools/open_model_zoo/tools/downloader/intel"
+    MODEL_PATH = "./models"
+    GRAPH_FILENAME = MODEL_PATH + "/" + MODEL_NAME
+    if RUNNING_ON_RPI:
+        GRAPH_FILENAME += "/FP16/"
+    else:
+        GRAPH_FILENAME += "/FP32/"
+    GRAPH_XML = GRAPH_FILENAME + MODEL_NAME + ".xml"
+    GRAPH_BIN = GRAPH_FILENAME + MODEL_NAME + ".bin"
+    net = cv2.dnn.readNet(GRAPH_XML, GRAPH_BIN)
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+    return net
+
+def check_optimization():
+    if not cv2.useOptimized():
+        cv2.setUseOptimized(True)
+
+def get_input(input_file):
+    if not input_file:
+        log("WARNING", "starting video stream.")
+        return VideoStream(src = 6).start()
+    log("WARNING", "opening video file.")
+    return cv2.VideoCapture(input_file)
+
 def parse_arguments():
     ap = argparse.ArgumentParser()
     ap.add_argument("-c", "--confidence",
@@ -158,72 +222,30 @@ def parse_arguments():
                     help = "Log message level. Default is set to 1, i.e. no 'DEBUG' level.")
     ap.add_argument("-i", "--input",
                     type = str,
+                    default = "",
                     help = "Path to optional input video file.")
     ap.add_argument("-o", "--output",
                     type = str,
+                    default = 0,
                     help = "Path to optional output video file.")
     args = vars(ap.parse_args())
     return args
 
 def main():
-    global W, H, log_level
+    global log_level
     args = parse_arguments()
+    show_flag = args['show']
     log_level = args['debug']
-    if not cv2.useOptimized():
-        cv2.setUseOptimized(True)
-    writer = None
+    input_file = args['input']
+    check_optimization()
     net = load_model()
-    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
-    if not args.get("input", False):
-        log("WARNING", "starting video stream.")
-        vs = VideoStream(src = 0).start()
-    else:
-        log("WARNING", "opening video file.")
-        vs = cv2.VideoCapture(args["input"])
+    vs = get_input(input_file)
     fps = FPS().start()
-    while True:
-        try:
-            frame = vs.read()
-            frame = frame[1] if args.get("input", False) else frame
-            H, W, _ = frame.shape
-            image_for_result = frame.copy()
-            if args["output"] is not None and writer is None:
-                    fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-                    writer = cv2.VideoWriter(args["output"], fourcc, 30,
-                            (frame.shape[1], frame.shape[0]), True)
-            predictions = predict(frame, net, args['confidence'])
-            for (i, pred) in enumerate(predictions):
-                label, pred_conf, pred_boxpts = pred
-                ((x_min, y_min), (x_max, y_max)) = pred_boxpts
-                log("DEBUG", "Prediction #%s: confidence: %s, boxpoints: %s" %\
-                             (i, pred_conf, pred_boxpts))
-                if pred_conf > args["confidence"]:
-                    log("DEBUG", "Prediction #%s: confidence: %s, boxpoint: %s" %\
-                                (i, pred_conf, pred_boxpts))
-                    if args["show"] > 0:
-                        print_label(label,
-                                    image_for_result,
-                                    x_min,
-                                    x_max,
-                                    y_min,
-                                    y_max,
-                                    pred_conf)
-            if writer is not None:
-                    writer.write(image_for_result)
-            if args["show"] > 0:
-                cv2.imshow("Output", image_for_result)
-                key = cv2.waitKey(1) & 0xFF
-                if key == ord("q"):
-                    break
-            fps.update()
-        except KeyboardInterrupt:
-            break
-        except AttributeError:
-            break
+    start_process(args, net, vs, show_flag, input_file, fps)
     fps.stop()
-    if args["show"] > 0:
+    if show_flag:
         cv2.destroyAllWindows()
-    if not args.get("input", False):
+    if not input_file:
         vs.stop()
     else:
         vs.release()
