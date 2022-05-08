a81c2ea5b0ea59fc8282f7fcd76874cc5db366d1
SergeyVasilyan <63066818+SergeyVasilyan@users.noreply.github.com>
2022-05-09 01:52:03 +0400

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+echo "deb https://apt.repos.intel.com/openvino/2022 focal main" | sudo tee /etc/apt/sources.list.d/intel-openvino-2022.list
+sudo apt update
+sudo apt install openvino
+sudo apt install openvino-opencv
